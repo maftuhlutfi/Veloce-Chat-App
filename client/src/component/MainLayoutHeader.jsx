61db91bb6c67b118c2cda2570944cecd4e7499ea
unknown <playmakernyomlo@gmail.com>
2020-12-04 20:05:44 +0700

    add leave button

--- client/src/component/MainLayoutHeader.jsx
+++ client/src/component/MainLayoutHeader.jsx
@@ -4,11 +4,15 @@ import './MainLayoutHeader.scss';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { leaveRoomStart } from "../redux/actions";
+import Button from './Button';
+import { useState } from 'react';
 
 function MainLayoutHeader(props) {
 	const dispatch = useDispatch();
 	const roomCode = useSelector(state => state.room.roomCode);
     const username = useSelector(state => state.user.username);
+
+    const [show, setShow] = useState(false);
     
     const handleClick = () => {
         dispatch(leaveRoomStart({roomCode, username}))
@@ -18,7 +22,10 @@ function MainLayoutHeader(props) {
         <header className='main-layout-header'>
             <div></div>
             <div className="three-dot">
-                <BsThreeDots onClick={handleClick} color='#c1c7da' size='1.5em' />
+                <BsThreeDots onClick={() => setShow(!show)} color='#c1c7da' size='1.5em' />
+                <div className="info" style={{display: show ? 'block' : 'none'}}>
+                    <Button onClick={handleClick} label='Leave room' style={{backgroundColor: 'red', color: 'white'}} />
+                </div>
             </div>
         </header>
     );
